3aa21b68de2aa50e0382507b92faee4957a6616f
leo.saban <leodsaban@outlook.com>
2025-03-26 08:33:12 -0600

    this is the start of the multiple test assignment to test hamburger menu and shopping cart

--- test/specs/login.specs.js
+++ test/specs/login.specs.js
@@ -2,21 +2,15 @@ import { expect } from '@wdio/globals';
 import loggingIn from '../pageobjects/loginTestPage.js';
 
 describe('User can log in', () => {
-    const usernames = ['standard_user', 'problem_user', 'performance_glitch_user','error_user', 'visual_user'];
-    const password = 'secret_sauce';
-
-    before(async () => {
-        await loggingIn.open(); // Open login page before running tests
-    });
-
-    usernames.forEach((username) => {
-        it(`should log in successfully as ${username}`, async () => {
-            await loggingIn.firstUserAttempt(username, password); // Logs in with the username and password
+           it('should log in successfully as', async () => {
+            await loggingIn.open(); // Opens the login page
+            await loggingIn.firstUserAttempt('standard_user', 'secret_sauce'); // Logs in with the username and password
             await loggingIn.hamburgerMenu(); // Opens the hamburger menu
             await loggingIn.logOut(); // Logs out
+            await expect(loggingIn.btnSubmit).toExist(); // Ensure user is logged out before next username attempt
         });
     });
-});
+ 
 
 
 
