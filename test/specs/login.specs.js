056ccefbfcc6803b676b4a03c7eabc52b4d43ff4
leo.saban <leodsaban@outlook.com>
2025-03-26 07:11:58 -0600

    removed some expects from the test file, and the negative test

--- test/specs/login.specs.js
+++ test/specs/login.specs.js
@@ -12,19 +12,8 @@ describe('User can log in', () => {
     usernames.forEach((username) => {
         it(`should log in successfully as ${username}`, async () => {
             await loggingIn.firstUserAttempt(username, password); // Logs in with the username and password
-            await expect(loggingIn.headerProducts).toExist(); // Validates the successful login
             await loggingIn.hamburgerMenu(); // Opens the hamburger menu
             await loggingIn.logOut(); // Logs out
-            await expect(loggingIn.btnSubmit).toExist(); // Ensure user is logged out before next username attempt
-        });
-
-    before(async () => {
-        await loggingIn.open(); // Open login page before running tests
-    });
-        it(`all correct logins should fail ${username}`, async () => {
-            await loggingIn.login(username, 'badpassword'); // Login attempt
-            await expect(loggingIn.failedMessage).toExist(); // Validate successful login
-            await expect(loggingIn.btnSubmit).toExist(); // Ensure user is logged out before next iteration
         });
     });
 });
