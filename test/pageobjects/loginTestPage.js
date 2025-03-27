4b543e7c766c3eff035fe26ee5c1e1db1cf34635
leo.saban <leodsaban@outlook.com>
2025-03-27 09:33:38 -0600

    Added more assertions to check the Cart page

--- test/pageobjects/loginTestPage.js
+++ test/pageobjects/loginTestPage.js
@@ -7,40 +7,57 @@ class loggingIn extends Page {
     get usernameInputField() {
         return $('//*[@id="user-name"]');
     }
+
     get passwordInputField() {
         return $('#password');
     }
+
     get btnSubmit () {
         return $('//*[@id="login-button"]');
     }
 
     get headerProducts () {
         return $('//span[@data-test="title"][contains(text(), "Products")]');
-    };
+    }
 
     get hamburgerMenuButton () {
         return $('//*[@id="react-burger-menu-btn"]');
     }
+
     get menu () {
         return $('.bm-menu');
     }
+
     get aboutText () {
         return $('//*[@id="about_sidebar_link"]');
     }
+
     get lockoutMessage () {
         return $('.error-button');
     }
+
     get failedMessage () {
         return $('//h3[contains(text(), "Epic sadface: Username and password do not match any user in this service")]');
     }
+
     get logOutButton () {
         return $('//*[@id="logout_sidebar_link"]');
     }
+
     get shoppingCartButton () {
         return $('//*[@id="shopping_cart_container"]');
     }
+
     get continueShoppingButton () {
         return $('//*[@id="continue-shopping"]');
+    }
+
+    get qtyElement () {
+        return $('//*[@class="cart_quantity_label"]')
+    }
+
+    get descriptionElement () {
+        return $('//*[@class="cart_desc_label"]')
     }
 
     async login (username, password) {
@@ -58,23 +75,26 @@ class loggingIn extends Page {
         await this.hamburgerMenuButton.click();
         await expect(this.menu).toBeDisplayed();
     }
+
     async about () {
         await this.aboutText.click();
         await expect(browser).toHaveUrl('https://saucelabs.com/')
     }
+
     async shoppingCartIcon () {
+        expect(this.shoppingcartIcon).toExist();
         await this.shoppingCartButton.click();
+        await expect(this.qtyElement).toHaveText('QTY');
+        await expect(this.descriptionElement).toHaveText('Description');
         await expect(browser).toHaveUrl('https://www.saucedemo.com/cart.html');
         await this.continueShoppingButton.click();
         await expect(browser).toHaveUrl('https://www.saucedemo.com/inventory.html');
     }
 
-
     async logOut () {
         await this.logOutButton.click();
         await expect(browser).toHaveUrl('https://www.saucedemo.com/');
     }
-
 
     open () {
         return super.open();
--- test/specs/login.specs.js
+++ test/specs/login.specs.js
@@ -1,22 +1,21 @@
-import { expect } from '@wdio/globals';
 import loggingIn from '../pageobjects/loginTestPage.js';
 
-describe('User can log in', () => {
-           it('should log in and display the Products page along with the hamburger menu', async () => {
-            await loggingIn.open(); // Opens the login page
-            await loggingIn.firstUserAttempt('standard_user', 'secret_sauce'); // Logs in with the username and password
-            await loggingIn.hamburgerMenu(); // Opens the hamburger menu
-            await loggingIn.about(); // Clicks on the about us button
-            await browser.back(); // Navigate back to the previous page
-            await loggingIn.open(); // Opens the login page          
-            await loggingIn.firstUserAttempt('standard_user', 'secret_sauce'); // Logs in with the username and password
-            await loggingIn.hamburgerMenu();
-            await loggingIn.logOut(); // Logs out
-        });
-    });
+// describe('User can log in', () => {
+//            it('should log in and display the Products page along with the hamburger menu', async () => {
+//             await loggingIn.open(); // Opens the login page
+//             await loggingIn.firstUserAttempt('standard_user', 'secret_sauce'); // Logs in with the username and password
+//             await loggingIn.hamburgerMenu(); // Opens the hamburger menu
+//             await loggingIn.about(); // Clicks on the about us button
+//             await browser.back(); // Navigate back to the previous page
+//             await loggingIn.open(); // Opens the login page          
+//             await loggingIn.firstUserAttempt('standard_user', 'secret_sauce'); // Logs in with the username and password
+//             await loggingIn.hamburgerMenu();
+//             await loggingIn.logOut(); // Logs out
+//         });
+//     });
  
     describe('shopping cart test', () => {
-        it('shopping cart contents', async () => {
+        it('All items inside the shopping cart', async () => {
          await loggingIn.open(); // Opens the login page
          await loggingIn.firstUserAttempt('standard_user', 'secret_sauce'); // Logs in with the username and password
          await loggingIn.shoppingCartIcon(); // Clicks on the shopping cart icon        
